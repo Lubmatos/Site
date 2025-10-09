778ccb9ea2a3f8faceb1c0016868fd971051416c
lubmatos <luizamatoscontatos@gmail.com>
2025-10-09 20:00:12 -0300

    Alteração Final, Envio Funcionando

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const form = document.querySelector("#contato form");
+    const form = document.querySelector("#contato form");
 
-  form.addEventListener("submit", function (event) {
-    event.preventDefault(); 
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
 
-    const nome = form.querySelector('input[placeholder="Nome"]').value;
-    const email = form.querySelector('input[placeholder="Email"]').value;
-    const mensagem = form.querySelector("textarea").value;
+        const nome = form.querySelector('input[placeholder="Nome"]').value;
+        const email = form.querySelector('input[placeholder="Email"]').value;
+        const mensagem = form.querySelector("textarea").value;
 
-    emailjs
-      .send("service_gv7upv6", "template_glveh72", {
-        from_name: nome,
-        from_email: email,
-        message: mensagem,
-      })
-      .then(function () {
-        alert("Recebido!");
-        form.reset(); 
-      });
-  });
+        emailjs.send("service_gv7upv6", "template_glveh72", {
+            from_name: nome,
+            from_email: email,
+            message: mensagem,
+        })
+        .then(function() {
+            alert("Recebido!");
+            form.reset();
+        })
+        .catch(function(error) {
+            console.log(error);
+            alert("Erro ao enviar!");
+        });
+    });
 });
